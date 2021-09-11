e32c82de633626f1a7d3df7d1f8d91ca26766d5a
levkotman <leoktmn@yandex.ru>
2021-09-12 01:23:27 +0200

    prepare to deploy to pages

---
+++ deploy.sh
@@ -0,0 +1,1 @@
+#!/usr/bin/env sh# abort on errorsset -e# buildyarn run build# navigate into the build output directorycd dist# if you are deploying to a custom domain# echo 'www.example.com' > CNAMEgit initgit add -Agit commit -m 'deploy'git push -f git@github.com:leokotman/monster-game.git master:gh-pagescd -
